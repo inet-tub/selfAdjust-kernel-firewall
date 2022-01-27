8b789fa3e436aee8d14a6663fa7c86e0879f04a8
Jonas Köppeler <j.koeppeler@tu-berlin.de>
2022-01-27 11:43:30 +0100

    comments added for memoryless implemenation

--- selfAdjustingList_generic/list_mrf_extension.h
+++ selfAdjustingList_generic/list_mrf_extension.h
@@ -5,6 +5,13 @@
 #ifndef SELFADJUSTINGLIST_GENERIC_LIST_MRF_EXTENSION_H
 #define SELFADJUSTINGLIST_GENERIC_LIST_MRF_EXTENSION_H
 #include <linux/list.h>
+/**
+ * list_access - memoryless implementation of the mrf- algorithm,
+ *               reorders elements in the list after an element has been accessed
+ * @pos: the element that is accessed
+ * @head: the head of the list
+ * @is_dependent: function which decides if two elements are dependent on each other
+ */
 static inline void
 list_access(struct list_head *pos, struct list_head *head, int(*is_dependent)(struct list_head *a, struct list_head *b))
 {
