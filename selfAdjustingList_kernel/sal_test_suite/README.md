6e06525ea2346e188068c927ac8bf6541fb45275
Jonas Köppeler <j.koeppeler@tu-berlin.de>
2022-03-14 16:36:26 +0100

    frontend to access the rule evaluation statistics

---
+++ selfAdjustingList_kernel/sal_test_suite/README.md
@@ -0,0 +1,4 @@
+install 
+
+libmnl
+libnftnl
--- selfAdjustingList_kernel/sal_test_suite/sal_test_control.c
+++ selfAdjustingList_kernel/sal_test_suite/sal_test_control.c
@@ -5,64 +5,47 @@
 #include <stdio.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include <libnftnl/chain.h>
+#include <libmnl/libmnl.h>
 #include <linux/netfilter/nfnetlink.h>
+#include <linux/netfilter/nf_tables.h>
+#include <linux/netfilter.h>
 
-
-#define NETLINK_NETFILTER 12
-#define PAYLOAD 32
 #define NFT_MSG_GETTRAVNODES 25
-#define NFNL_SUBSYS_NFTABLES 10
 
-    struct sockaddr_nl src_addr, dest_addr;
-    struct nlmsghdr *nlh = NULL;
-    struct iovec iov;
-    int sock_fd;
-    struct msghdr msg;
-    struct nfgenmsg genmsg;
-    char * table_name = "my_tab";
-    struct nlattr tab_n;
-    
+
 int main(){
-    sock_fd = socket(PF_NETLINK, SOCK_RAW, NETLINK_NETFILTER);
-    if(sock_fd < 0)
-        return -1;
+    struct mnl_socket *nl;
+    char buf [MNL_SOCKET_BUFFER_SIZE];
+    struct nlmsghdr *nlh;
+    struct nfgenmsg *genmsg;
+    struct nlattr *attr;
+    //uint32_t portid, type = NFTNL_OUTPUT_DEFAULT;
+    struct nftnl_chain *t = NULL;
+    int ret;
+    int family;
 
-    memset(&src_addr, 0 , sizeof(src_addr));
-    src_addr.nl_family = AF_NETLINK;
-    src_addr.nl_pid = getpid();
+    family = NFPROTO_IPV4;
+    t = nftnl_chain_alloc();
+    nlh = nftnl_chain_nlmsg_build_hdr(buf, NFT_MSG_GETTRAVNODES, family, 0, 0);
+    nftnl_chain_set_str(t, NFTNL_CHAIN_TABLE, "my_tab");
+    nftnl_chain_set_str(t, NFTNL_CHAIN_NAME, "output_chain");
+    nftnl_chain_nlmsg_build_payload(nlh, t);
 
-    bind(sock_fd, (struct sockaddr *)&src_addr, sizeof(src_addr));
+    nl = mnl_socket_open(NETLINK_NETFILTER);
+    mnl_socket_bind(nl, 0, MNL_SOCKET_AUTOPID);
+    //portid = mnl_socket_get_portid(nl);
+    mnl_socket_sendto(nl, nlh, nlh->nlmsg_len);
+    ret  = mnl_socket_recvfrom(nl, buf, sizeof(buf));
+    if(ret > 0){
+        nlh =(void *) buf;
+        printf("len: %u type: %hu, flags: %hu, nlmsg_seq: %u, portid: %u\n", nlh->nlmsg_len, nlh->nlmsg_type, nlh->nlmsg_flags, nlh->nlmsg_seq, nlh->nlmsg_pid);
+        genmsg = NLMSG_DATA(nlh);
+        printf("family %u, version %d, res_id %d\n", genmsg->nfgen_family, genmsg->version, genmsg->res_id);
+        attr = (void *) (genmsg + 1);
+        uint32_t *nodes = (void *)(attr+1);
+        printf("len %hu type %hu %u\n", attr->nla_len, attr->nla_type, ntohl(*nodes));
+    }
 
-    memset(&dest_addr, 0, sizeof(dest_addr));
-    dest_addr.nl_family = AF_NETLINK;
-
-    memset(&genmsg, 0, sizeof(genmsg));
-    genmsg.nfgen_family = AF_INET;
-
-    memset(&tab_n, 0, sizeof(tab_n));
-    tab_n.nla_type = 5;
-    tab_n.nla_len = strlen(table_name);
-
-    unsigned int payload = sizeof(struct nfgenmsg) + sizeof(struct nlattr) + tab_n.nla_len;
-    nlh = (struct nlmsghdr *)malloc(NLMSG_SPACE(payload));
-    memset(nlh, 0, NLMSG_SPACE(payload));
-    nlh->nlmsg_len = NLMSG_SPACE(payload);
-    nlh->nlmsg_flags |= NLM_F_REQUEST;
-    nlh->nlmsg_type = (NFNL_SUBSYS_NFTABLES<<8) | NFT_MSG_GETTRAVNODES;
-    nlh->nlmsg_pid = getpid();
-    memcpy(NLMSG_DATA(nlh), &genmsg, sizeof(genmsg));
-    memcpy(NLMSG_DATA(nlh)+sizeof(genmsg), &tab_n, sizeof(tab_n));
-    strncpy(NLMSG_DATA(nlh)+ sizeof(genmsg)+ sizeof(tab_n), table_name, strlen(table_name));
-
-
-    iov.iov_base = (void *)nlh;
-    iov.iov_len = nlh->nlmsg_len;
-    msg.msg_name = (void *)&dest_addr;
-    msg.msg_namelen = sizeof(dest_addr);
-    msg.msg_iov = &iov;
-    msg.msg_iovlen = 1;
-    sendmsg(sock_fd, &msg, 0);
-    close(sock_fd);
-
-
+    mnl_socket_close(nl);
 }
