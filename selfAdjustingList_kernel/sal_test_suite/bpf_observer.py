ad6888e31da33cceeec4b9fe4bdffe54a6fb9ccb
Jonas Köppeler <j.koeppeler@tu-berlin.de>
2022-03-14 22:30:08 +0100

    exchange rule data over hash table

--- selfAdjustingList_kernel/sal_test_suite/bpf_observer.py
+++ selfAdjustingList_kernel/sal_test_suite/bpf_observer.py
@@ -3,6 +3,13 @@ from bcc.utils import printb
 prog = """
 #include <net/netfilter/nf_tables.h>
 #include <linux/types.h>
+struct rule_handle_t{
+	char ctrl;
+	u64 handle;
+};
+
+BPF_PERF_OUTPUT(trav_rules);
+
     unsigned int kprobe__nft_do_chain(struct pt_regs *ctx, struct nft_pktinfo *pkt, void *priv) {
         const struct nft_chain *chain = priv;
         const struct net *net = pkt->xt.state->net;
@@ -11,27 +18,54 @@ prog = """
         unsigned long *prio;
         int genbit = net->nft.gencursor;
         int i = 0;
-        bpf_trace_printk("Hello World %u \\n", chain->traversed_rules);
+        struct rule_handle_t data;
+        data.ctrl = 's';
+        data.handle = 0;
+        trav_rules.perf_submit(ctx, &data, sizeof(data));
+         
+        //bpf_trace_printk("Hello World %u \\n", chain->traversed_rules);
        	if(genbit){
-       		bpf_trace_printk("genbit on\\n");
+       		//bpf_trace_printk("genbit on\\n");
        		rules = chain->rules_gen_1;
        	}else{
-       		bpf_trace_printk("genbit off\\n");
+       		//bpf_trace_printk("genbit off\\n");
        		rules = chain->rules_gen_0;
        	}
+       	
+       	data.ctrl = 'r';
+       	
        	rule = *rules;
        	for(i = 0; i < 2048 ;++i, ++rules){
        		rule = *rules;
        		if(!rule)
        			break; 
-       		
-       		bpf_trace_printk("%lu \\n", rule->cmp_data.priority);
+       		data.handle = rule->cmp_data.priority;
+       		trav_rules.perf_submit(ctx, &data, sizeof(data));
+       		//bpf_trace_printk("%lu \\n", rule->cmp_data.priority);
        	}
-       	       	
+       	
+       	data.ctrl = 'e';
+       	data.handle = 0;
+       	trav_rules.perf_submit(ctx, &data, sizeof(data));       	
+       	
         return 0;
     }
 """
 b = BPF(text=prog)
-b.trace_print()
+
+def print_trav_rules(cpu, data, size):
+	rule_data = b["trav_rules"].event(data)
+	if rule_data.ctrl == b's':
+		print("BEGIN")
+	elif rule_data.ctrl == b'r':
+		print(rule_data.handle)
+	elif rule_data.ctrl == b'e':
+		print("END")
+
+b["trav_rules"].open_perf_buffer(print_trav_rules)
+while 1:
+	b.perf_buffer_poll()
+
+# b.trace_print()
 # prio = (unsigned long *)(((char *)rule) + sizeof(struct list_head));
 # bpf_trace_printk("%p %d", (prio), i);
