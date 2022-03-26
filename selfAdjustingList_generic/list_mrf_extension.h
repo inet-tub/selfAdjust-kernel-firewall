f7203f83a76530e2f63156c3244419521632a2dc
Jonas Köppeler <j.koeppeler@tu-berlin.de>
2022-03-26 11:45:42 +0100

    swaps were calculated wrongly. If pos is already the first element, it does not need to be moved again to lists head, since its already there. Therefore the swap counter does not need to be increased

--- selfAdjustingList_generic/list_mrf_extension.h
+++ selfAdjustingList_generic/list_mrf_extension.h
@@ -13,20 +13,19 @@
  * @is_dependent: function which decides if two elements are dependent on each other (a is a dependency of b)
  */
 static inline unsigned int
-list_access(struct list_head *pos, struct list_head *head, int(*is_dependent)(struct list_head *a, struct list_head *b))
-{
+list_access(struct list_head *pos, struct list_head *head, int(*is_dependent)(struct list_head *a, struct list_head *b)) {
     struct list_head *cur;
     unsigned int swap_count = 0;
-    if(list_is_first(pos, head))
+    if (list_is_first(pos, head))
         return 0;
     cur = pos;
 
-    while (!list_is_first(cur, head)){
+    while (!list_is_first(cur, head)) {
         cur = cur->prev;
         //cur is a dependency of pos => pos is not allowed to be in front of cur
         //move pos behind cur and set pos to cur
-        if(is_dependent(cur, pos)) {
-            if(cur != pos->prev) {
+        if (is_dependent(cur, pos)) {
+            if (cur != pos->prev) {
                 list_move(pos, cur);
                 swap_count++;
             }
@@ -34,8 +33,11 @@ list_access(struct list_head *pos, struct list_head *head, int(*is_dependent)(st
         }
     }
 
-    list_move(pos, head);
-    swap_count++;
+    if (!list_is_first(pos, head)){
+        list_move(pos, head);
+        swap_count++;
+    }
+
     return swap_count;
 }
 
