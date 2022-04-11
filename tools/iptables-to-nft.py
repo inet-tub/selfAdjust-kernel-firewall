3faeb50013cb5aa512e4d4b293aab18f6efe8e4d
Jonas Köppeler <j.koeppeler@tu-berlin.de>
2022-04-11 16:34:21 +0200

    writing output to file
    Signed-off-by: Jonas Köppeler <j.koeppeler@tu-berlin.de>

--- tools/iptables-to-nft.py
+++ tools/iptables-to-nft.py
@@ -3,8 +3,8 @@ import subprocess
 import sys
 
 def main():
-    if len(sys.argv) != 2:
-        print("iptables_to_nft <file from classbench_to_iptables>")
+    if len(sys.argv) != 3:
+        print("iptables_to_nft <file from classbench_to_iptables> <outputfile>")
         sys.exit(1)
 
     nft_str = "add table filter\nadd chain filter INPUT {type filter hook prerouting priority 0;}\n"
@@ -17,7 +17,9 @@ def main():
             line = subprocess.check_output(cmd).decode("utf-8")            
             line = line.replace("nft", "").replace("counter","").lstrip()
             nft_str += line
-    print(nft_str)
+# print(nft_str)
+    with open(sys.argv[2], "w") as f:
+        f.write(nft_str)
 
 
 if __name__ == '__main__':
